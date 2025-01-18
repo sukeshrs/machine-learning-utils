701f2f1296a3eb5325e47bd74f65d668034f61e0
sukeshrs <sukesrs@gmail.com>
2025-01-18 09:52:28 -0800

    Added code to do similarity search

--- document_based/wiki_document_load.py
+++ document_based/wiki_document_load.py
@@ -5,7 +5,7 @@ import os
 
 from util import chunk_docs, create_vector_store
 
-# Specify the Wikipedia page title
+# Specify the wiki page title
 loader = WikipediaLoader("Machu Picchu")
 documents = loader.load()
 
@@ -13,5 +13,12 @@ print(documents)
 
 chunked_docs = chunk_docs(documents,2000)
 v_store = create_vector_store(chunked_docs)
-print(v_store)
+#print(v_store)
 
+# Query the vector store
+query = "Where is Machu pichu?"
+results = v_store.similarity_search(query, k=3)
+
+# Display results
+for i, result in enumerate(results):
+    print(f"Result {i+1}:\n{result.page_content}\n")
