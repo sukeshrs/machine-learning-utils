059f2629a84b2dda8d66284f26f74682778f64e7
sukeshrs <sukesrs@gmail.com>
2025-01-17 21:01:51 -0800

    Added new util file and wiki loader

--- document_based/pdf_document_load.py
+++ document_based/pdf_document_load.py
@@ -28,8 +28,7 @@ def create_vector_store(chunked_docs):
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-
-    vector_store = FAISS.from_documents(documents, embeddings)
+    vector_store = FAISS.from_documents(chunked_docs, embeddings)
     return vector_store
 
 def get_qa_chain():
--- document_based/wiki_document_load.py
+++ document_based/wiki_document_load.py
@@ -1,14 +1,17 @@
 from langchain_community.document_loaders import WikipediaLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_community.chat_models import ChatOpenAI
 import os
 
+from util import chunk_docs, create_vector_store
 
 # Specify the Wikipedia page title
 loader = WikipediaLoader("Machu Picchu")
 documents = loader.load()
 
 print(documents)
+
+chunked_docs = chunk_docs(documents,2000)
+v_store = create_vector_store(chunked_docs)
+print(v_store)
+
